e133308adf316d1efdfebd7c7060730caa54f415
lan.lin <lan.lin@heitao.com>
2020-09-23 16:30:35 +0800

    nacos client日志路径设置只能通过jvm -D方式设置

--- src/main/resources/startup.sh
+++ src/main/resources/startup.sh
@@ -1,1 +1,1 @@
-nohup java -Dcsp.sentinel.config.file=config/sentinel.properties -Dproject.name=sentinel-dashboard -jar sentinel-dashboard.jar &
+nohup java -Dnacos.logging.path=logs/nacos/ -Dcsp.sentinel.config.file=config/sentinel.properties -Dproject.name=sentinel-dashboard -jar sentinel-dashboard.jar &
